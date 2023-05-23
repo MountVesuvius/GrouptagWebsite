6f73867a0c244ea05765f2bfc9b6cd350801d859
MountVesuvius <JudeALakkis@gmail.com>
2023-05-23 16:44:29 +1000

    Add Student Clubs page

---
+++ src/routes/clubs/+page.svelte
@@ -0,0 +1,48 @@
+<section class="text-white ">
+    <div class="bg-eggshell flex justify-center items-center text-center px-10 py-16">
+        <h1 class="text-black font-black text-3xl sm:text-[4rem] sm:leading-tight sm:px-[20%]">Run, manage and grow your student club with <span class="text-grouptag">Grouptag Groups</span></h1>
+    </div>
+    <!-- Rounded top buffer. need to still figure out how to style this -->
+    <div class="bg-accent-blue h-10 "></div>
+
+    <div class="bg-accent-blue px-10 space-y-5 flex flex-col sm:flex-row sm:px-[10%]">
+        <div class="sm:mr-10 sm:w-[50%] sm:pt-10">
+            <h2 class="mb-5 text-4xl">Reach & visibility are important concerns for student club executives.</h2>
+            <p class="text-xl">Grouptag Groups organises communities better to make it easier and faster to reach the right students.</p>
+            <button class="invisible sm:visible bg-blue-900 sm:px-10 sm:py-3 rounded-xl font-bold mt-5">Try Groups</button>
+        </div>
+        <div class="pb-24">
+            <img class="rounded-xl shadow-3xl" src="images/Education-Header.webp" alt="Education Header">
+        </div>
+    </div>
+</section>
+
+<!-- Took some creative license with this section. Images suck to position -->
+<section class="text-black px-10 pt-8 sm:pt-0 sm:flex">
+    <div class="sm:mt-64 sm:w-1/2 sm:ml-24">
+        <h2>Engagement is a strong driver of your club's performance.</h2>
+        <p class="text-xl">Whether it's public or private – user profiles give your members a deeper insight into who they are connecting with.</p>
+        <button class="invisible sm:visible bg-blue-900 sm:px-10 sm:py-3 rounded-xl font-bold mt-5 text-white">Check out Profiles</button>
+    </div>
+
+    <div class="relative h-[85vh] overflow-hidden">
+        <div class="relative p-10 sm:p-0">
+            <img class="sm:invisible absolute left-[35vw] rounded-xl w-48 shadow-3xl
+                sm:left-[0vh] sm:top-[0vh]"
+                src="images/Student-Group.webp" alt="Group of students">
+            <img class="sm:invisible absolute top-[10vh] -left-[14vw]
+                sm:top-[0vh] sm:left-[0vh]"
+            src="images/Down-Arrow.webp" alt="Downwards Arrow">
+        </div>
+        <img class="rounded-xl shadow-3xl scale-75 mt-[25vh] sm:mt-0" src="images/Student-Profile.webp" alt="Screenshot of a Grouptag student profile">
+    </div>
+</section>
+
+<section class="text-black bg-[#e5f0fe] px-10 pt-10 flex flex-col sm:flex-row sm:px-[15%] sm:py-[10%] sm:justify-center sm:items-center">
+    <div>
+        <h2 class="mb-3">Growth is better than scale when it comes to building your community.</h2>
+        <p>Scale sounds great. You can chuck a couple Facebook ads up and increase your member base by 2-5x, but members remain distant. Grouptag turns your member-base into your marketing engine by fostering closer connection and networking.</p>
+        <button class="invisible sm:visible bg-blue-900 sm:px-10 sm:py-3 rounded-xl font-bold mt-5 text-white">Try Grouptag Groups</button>
+    </div>
+    <img class="scale-90" src="images/Icon-Stack.webp" alt="Row of icons: Search, Wave, Send, Add">
+</section>
