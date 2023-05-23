6783f1d149f9394b0b8114707fda6f4bbe0d45dd
MountVesuvius <JudeALakkis@gmail.com>
2023-05-23 16:42:40 +1000

    Added style extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    
+    extend: {
+      colors: {
+        'grouptag': '#46cdff',
+        'primary': '#0c2030',
+        'eggshell': '#fafafa',
+        'accent-green': '#15291b',
+        'accent-blue': '#00a0ff'
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: []
 };
